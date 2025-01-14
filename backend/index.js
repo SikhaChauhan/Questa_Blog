83b0f8c4be8ea626125496b783ad9418330bc8be
SikhaChauhan <csikha1010@gmail.com>
2025-01-14 21:15:04 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -79,6 +79,14 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, 'public'), {
+//   setHeaders: (res, filePath) => {
+//       if (filePath.endsWith('.js')) {
+//         res.setHeader('Content-Type', 'application/javascript'); 
+//       }
+//   }
+// }));
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
